a5ea3c5a2edcfc08a37b59ff7525eb4b800c7de8
nathan <nlm105@francis.edu>
2020-03-03 16:10:32 -0500

    Added preprocessing call Add feature selection analysis commented out other models except for XGBC for feature testing

--- test_predict_3.py
+++ test_predict_3.py
@@ -18,7 +18,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import mutual_info_classif
 from CSV_Format import csv_format
+import pre_processing
 
 desired_width = 100
 pd.set_option('display.width', desired_width)
@@ -35,6 +39,10 @@ YN = ['Enrolled', 'Home State of PA', 'Home County of Cambria', 'Gender', 'Admit
 
 # call to reformat the data frame
 df = csv_format(df, cat_names, cont_names, YN)
+
+df = pre_processing.pre_process(df)
+print(df.Country.value_counts())
+
 
 #create dummy variables for categorical data
 cat_vars = df[cat_names]
@@ -92,18 +100,9 @@ def stats(model):
     # predict_train = model.predict(train_x)
     # print('Target on train data',predict_train)
 
-    # Accuray Score on train dataset
-    # accuracy_train = accuracy_score(train_y,predict_train)
-    # print('accuracy_score on train dataset : ', accuracy_train)
-
     # predict the target on the test dataset
     predict_train = model.predict(train_x)
     predict_test = model.predict(test_x)
-    # print('Target on test data',predict_test)
-
-    # Accuracy Score on test dataset
-    # accuracy_test = accuracy_score(test_y,predict_test)
-    # print('accuracy_score on test dataset : ', accuracy_test)
 
     #Confusion Matrix
     print(confusion_matrix(test_y, predict_test))
@@ -111,21 +110,88 @@ def stats(model):
     print("Accuracy:",metrics.accuracy_score(test_y, predict_test))
     print("Precision:",metrics.precision_score(test_y, predict_test))
     print("Recall:",metrics.recall_score(test_y, predict_test))
+    print("F1 score:", metrics.f1_score(test_y,predict_test))
 
-    # prob = model.predict_proba(test_x)
-    # p = pd.DataFrame(prob, columns=model.classes_)
-    # print(p)
+    fpr, tpr, thresholds = metrics.roc_curve(test_y, predict_test)
+    print('AUC: %.3f' % metrics.roc_auc_score(test_y,predict_test))
+
+    # generate a no skill prediction (majority class)
+    ns_probs = [0 for _ in range(len(test_y))]
+    # predict probabilities
+    lr_probs = model.predict_proba(test_x)
+    # keep probabilities for the positive outcome only
+    lr_probs = lr_probs[:, 1]
+    # calculate scores
+    ns_auc = metrics.roc_auc_score(test_y, ns_probs)
+    lr_auc = metrics.roc_auc_score(test_y, lr_probs)
+    # summarize scores
+    print('No Skill: ROC AUC=%.3f' % (ns_auc))
+    print('Model: ROC AUC=%.3f' % (lr_auc))
+    # calculate roc curves
+    ns_fpr, ns_tpr, _ = metrics.roc_curve(test_y, ns_probs)
+    lr_fpr, lr_tpr, _ = metrics.roc_curve(test_y, lr_probs)
+    # plot the roc curve for the model
+    plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+    plt.plot(lr_fpr, lr_tpr, marker='.', label='Model')
+    # axis labels
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the legend
+    plt.legend()
+    # show the plot
+    plt.show()
+
+    # calculate precision-recall curve
+    precision, recall, thresholds = metrics.precision_recall_curve(test_y, predict_test)
+    #plot the precision-recall curve
+    no_skill = len(test_y[test_y==1]) / len(test_y)
+    plt.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
+    plt.plot(recall, precision, marker='.', label='Model')
+    # axis labels
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    # show the legend
+    plt.legend()
+    # show the plot
+    plt.show()
 
 
-print('Logistic Regression')
-stats(LogModel)
-# stats(SVCModel)
-print("K Nearest Neighbors")
-stats(KNN)
-# stats(KM)
-print("Random Forest")
-stats(forest)
-print("Gradient Boosting")
-stats(GBoost)
-print('XGBC')
-stats(XGBC)
+
+    print()
+
+
+# models= {"Logistic Regression": LogModel, "K Nearest Neighbors": KNN, "Random Forest": forest, "Gradient Boosting":GBoost, "XGBC": XGBC}
+# print('Logistic Regression')
+# stats(LogModel)
+# # stats(SVCModel)
+# print("K Nearest Neighbors")
+# stats(KNN)
+# # stats(KM)
+# print("Random Forest")
+# stats(forest)
+# print("Gradient Boosting")
+# stats(GBoost)
+# print('XGBC')
+# stats(XGBC)
+
+# feature selection
+def select_features(X_train, y_train, X_test):
+    fs = SelectKBest(score_func=mutual_info_classif, k='all')
+    fs.fit(X_train, y_train)
+    X_train_fs = fs.transform(X_train)
+    X_test_fs = fs.transform(X_test)
+    return X_train_fs, X_test_fs, fs
+
+
+print("XGBC testing")
+XGBC.fit(test_x,test_y)
+# print(XGBC.feature_importances_)
+# print(X.columns)
+
+# X_train_fs, X_test_fs, fs = select_features(train_x, train_y, test_x)
+
+for name, feature in zip(X.columns, XGBC.feature_importances_):
+    print("%s: importance:%.3f" %(name,feature))
+
+# plt.bar(range(len(XGBC.feature_importances_)), XGBC.feature_importances_)
+# plt.show()
