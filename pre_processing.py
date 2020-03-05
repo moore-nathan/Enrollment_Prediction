372f4ab311dbf3a6c9e3cdd3253fb6b1486c7311
nathan <nlm105@francis.edu>
2020-03-04 22:34:29 -0500

    Removed comments and print statements

--- pre_processing.py
+++ pre_processing.py
@@ -23,9 +23,11 @@ def pre_process(df):
         df.loc[:, 'State'] = df.loc[:, 'State'].replace(dict.fromkeys(West_South_Central, 'West_South_Central'))
         df.loc[:, 'State'] = df.loc[:, 'State'].replace(dict.fromkeys(Mountain, 'Mountain'))
         df.loc[:, 'State'] = df.loc[:, 'State'].replace(dict.fromkeys(Pacific, 'Pacific'))
-
         # Ask if rest of inputs in State are Canadian?
+
+
     def europe_code():
+        # found code that made this inferior, but still interesting so function was kept
         e_url = 'https://pkgstore.datahub.io/opendatafortaxjustice/listofeucountries/listofeucountries_csv/data/5ab24e62d2ad8f06b59a0e7ffd7cb556/listofeucountries_csv.csv'
         e = pd.read_csv(e_url)
         # print(e.head())
@@ -48,11 +50,8 @@ def pre_process(df):
         }
         countries = list(pycountry.countries)
         countries = [x.name for x in countries]
-        # print(countries)
         countries = [x for x in countries if x not in ['United States', 'France', 'Canada']]
-        # print('United States' in countries)
         # country_codes = [pc.country_name_to_country_alpha2(x, cn_name_format="default") for x in countries]
-        # print(country_codes)
         cont = []
         for country in countries:
             try:
@@ -61,10 +60,8 @@ def pre_process(df):
             except:
                 cont.append('NONE')
         # con = [continents[pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(country))] for country in countries]
-        # print(cont)
         countries = [x.upper() for x in countries]
         continents = dict(zip(countries, cont))
-        # print(continents)
 
         df.loc[:, 'Country'] = df.loc[:, 'Country'].replace(continents)
         # some names are off because they include full title but this works
