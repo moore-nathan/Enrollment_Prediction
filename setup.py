fd23efa7ff77addc71c4bebdc20778423ee21ef0
nathan <nlm105@francis.edu>
2020-03-12 18:05:35 -0400

    Creation of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Enrollment_Prediction',
+    version='1.1',
+    packages=['pandas', 'numpy',"sklearn","xgboost",'matplotlib',
+              'pycountry_convert','pycountry'],
+    url='https://github.com/whitecheeks33/Enrollment_Prediction',
+    license='',
+    author='Nathan Moore',
+    author_email='cheeksofwhite@gmail.com',
+    description='Enrollment Prediction for Saint Francis University'
+)
