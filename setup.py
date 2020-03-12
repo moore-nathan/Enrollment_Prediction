e8a62aee9feb7d3e5d553423c41efb9de5d284ef
nathan <nlm105@francis.edu>
2020-03-12 18:50:53 -0400

    Reformatting the structure of Project

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(
     name='Enrollment_Prediction',
     version='1.1',
-    packages=['pandas', 'numpy',"sklearn","xgboost",'matplotlib',
-              'pycountry_convert','pycountry'],
+    packages=['Enrollment_Prediction'],
     url='https://github.com/whitecheeks33/Enrollment_Prediction',
-    license='',
+    license='SFU',
     author='Nathan Moore',
     author_email='cheeksofwhite@gmail.com',
-    description='Enrollment Prediction for Saint Francis University'
+    description='Enrollment Prediction for Saint Francis University',
+    install_requires = ['pandas', 'numpy',"sklearn","xgboost",'matplotlib',
+              'pycountry_convert','pycountry']
 )
