6051b7bc512c2e452b8deae9697d7bd746750d8c
nathan <nlm105@francis.edu>
2020-03-19 23:08:01 -0400

    Attempted to get setup.py working. Still needs help

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Enrollment_Prediction',
     version='1.1',
-    packages=['Enrollment_Prediction'],
+    packages=find_packages(),
     url='https://github.com/whitecheeks33/Enrollment_Prediction',
     license='SFU',
     author='Nathan Moore',
     author_email='cheeksofwhite@gmail.com',
     description='Enrollment Prediction for Saint Francis University',
-    install_requires = ['pandas', 'numpy',"sklearn","xgboost",'matplotlib',
-              'pycountry_convert','pycountry'],
+    install_requires=['pandas', 'numpy', "sklearn", "xgboost", 'matplotlib',
+                      'pycountry_convert', 'pycountry'],
     scripts=['Enroll_script.py'],
 )
